3630f8644f88cc2bd24515949e58e84e005e1e68
christianlewis024 <58643162+christianlewis024@users.noreply.github.com>
2020-04-17 08:42:13 -0700

    setup folders and files

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleWare } from "redux";
 
 ReactDOM.render(<App />, document.getElementById("root"));
